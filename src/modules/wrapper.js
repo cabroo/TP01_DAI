9dc6b444e2afbb7bd1d2c4404718264bcdadae4d
Android Studio <devandroid@ort>
2025-03-31 11:38:41 -0300

    Ej04

--- src/modules/wrapper.js
+++ src/modules/wrapper.js
@@ -1,46 +1,80 @@
 import axios from "axios";
 const APIKEY = "568b3ac0"; 
 const OMDBSearchByPage = async (searchText, page = 1) => {
-let returnObject = {
-respuesta : false,
-cantidadTotal : 0,
-datos : []
+
+  let returnObject = {
+
+      respuesta     : false,
+
+      cantidadTotal : 0,
+
+      datos         : []
+
+    };
+
+
+    const requestString = `http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchText}&page=${page}`;
+    const apiResponse = await axios.get(requestString);
+
+    if (apiResponse.data.Response === "True") {
+      returnObject.respuesta = true;
+      returnObject.cantidadTotal = apiResponse.data.totalResults;
+      returnObject.datos = apiResponse.data.Search;
+    } else 
+    {
+      returnObject.respuesta = false;
+    }
+    } 
+    catch (error) 
+    {
+        console.error("Error al hacer la solicitud:", error);
+    }
+
+    return returnObject;
+
 };
 
-let respuesta = await Test();
-console.log('respuesta', respuesta);
-const Test = async (searchText) => {
-const requestString = `http://www.omdbapi.com/?apikey=7c62gb5e&s=cars`;
-const apiResponse = await axios.get(requestString);
-return apiResponse.data;
 
-// No seas vago, acá hay que hacer el cuerpo de la función!!!
-return returnObject;
-};
 const OMDBSearchComplete = async (searchText) => {
-let returnObject = {
-respuesta : false,
-cantidadTotal : 0,
-datos : []
+
+  let returnObject = {
+
+      respuesta     : false,
+
+      cantidadTotal : 0,
+
+      datos         : []
+
+    };
+
+
+  // No seas vago, acá hay que hacer el cuerpo de la función!!!
+
+  return returnObject;
+
 };
-// No seas vago, acá hay que hacer el cuerpo de la función!!!
-return returnObject;
-};
+
+
 const OMDBGetByImdbID = async (imdbID) => {
-let returnObject = {
-respuesta : false,
-cantidadTotal : 0,
-datos : {}
+
+  let returnObject = {
+
+      respuesta     : false,
+
+      cantidadTotal : 0,
+
+      datos         : {}
+
+    };
+
+
+  // No seas vago, acá hay que hacer el cuerpo de la función!!!
+
+  return returnObject;
+
 };
-// No seas vago, acá hay que hacer el cuerpo de la función!!!
-return returnObject;
-};
+
+
 // Exporto todo lo que yo quiero exponer del módulo:
+
 export {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID};
-
-
-
-
-
-
-};
