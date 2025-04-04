52731212c124c12be3e6df896fb3213b25c969ac
Android Studio <devandroid@ort>
2025-04-04 09:17:58 -0300

    arreglos

--- src/modules/omdb-wrapper.js
+++ src/modules/omdb-wrapper.js
@@ -35,31 +35,29 @@ const OMDBSearchByPage = async (searchText, page = 1) => {
 
 const OMDBSearchComplete = async (searchText) => {
 
-  let returnObject = {
-
-      respuesta     : false,
-
-      cantidadTotal : 0,
-
-      datos         : []
-
-    };
-
-    try {
-      const requestString = `http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchText}`;
-      const apiResponse = await axios.get(requestString);
+  let returnObject = { }; 
+  let currentPage = 1, maxPages, countResults;  
+  let returnPageTemp; 
   
-      if (apiResponse.data.Response === "True") {
-        returnObject.respuesta = true;
-        returnObject.cantidadTotal = apiResponse.data.totalResults;
-        returnObject.datos = apiResponse.data.Search;
-      } else {
-        returnObject.respuesta = false;
-      }
-    } catch (error) {
-      console.error("Error al hacer la solicitud:", error);
-    }
-  return returnObject;
+  returnPageTemp = await OMDBSearchByPage(searchText, currentPage); 
+  countResults = returnPageTemp.cantidadTotal;  
+  if(returnPageTemp.cantidadTotal <= RESULT_PER_PAGE)
+  {
+    returnObject = returnPageTemp; 
+  } 
+  else 
+  {
+    returnObject = returnPageTemp; 
+  } 
+
+   maxPages = Math.ceil(countResults / RESULT_PER_PAGE);  
+   for ( let currentPage = 2; currentPage <= maxPages; currentPage++ ) 
+   {
+     returnPagetemp = await OMDBSearchByPage(searchText, currentPage); 
+     returnObject.datos = returnObject.datos.concat(returnPageTemp.datos);  
+
+   } 
+   return returnObject; 
 
 };
 
